8dfd7d0032ca74f95717a7bbacc9088e10c7a1de
sambit420 <sambit2024@gmail.com>
2022-09-23 01:22:54 +0530

    Added in : Sort the results to show the Computation Value in Descending Order

--- Sorting_Arithmatic_Computation.sh
+++ Sorting_Arithmatic_Computation.sh
@@ -29,3 +29,26 @@ do
 done
 
 echo ${array[@]}
+
+size=${#array[@]}
+for(( i = 0; i < size - 1; i++ ))
+do
+    swapped=0
+    for(( j = 0; j < size - i - 1; j++))
+    do
+        if((array[j] < array[j+1]))
+        then
+            temp=${array[j]}
+            array[j]=${array[j+1]}
+            array[j+1]=$temp
+            swapped=1
+        fi
+    done
+    if((swapped==0))
+    then
+        break
+    fi
+done
+
+echo "Results sorted in descending order: "
+echo ${array[@]}
