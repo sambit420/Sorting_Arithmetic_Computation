d8086765055bd1f4202889e13d78014598772bc5
sambit420 <sambit2024@gmail.com>
2022-09-23 00:36:58 +0530

    Added in : First Computation (a + b * c)

--- Sorting_Arithmatic_Computation.sh
+++ Sorting_Arithmatic_Computation.sh
@@ -2,3 +2,5 @@ read -p "Enter 1st number: " a
 read -p "Enter 2nd number: " b
 read -p "Enter 3rd number: " c
 echo "Three numbers are: $a $b $c"
+
+echo "First Compute : a + b * c = $(( a + b * c ))"
