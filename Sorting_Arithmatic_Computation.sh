2dd96ca03e27a1db15266fa5971bea198efa52c2
sambit420 <sambit2024@gmail.com>
2022-09-23 01:00:20 +0530

    Added in : Store the Result in Dictionary for Every Computation

--- Sorting_Arithmatic_Computation.sh
+++ Sorting_Arithmatic_Computation.sh
@@ -7,3 +7,19 @@ echo "First Compute : a + b * c = $(( a + b * c ))"
 echo "Second Compute : a * b + c = $(( a * b + c ))"
 echo "Third Compute : c + a / b = $(( c + a / b ))"
 echo "Fourth Compute : a % b + c = $(( a % b + c ))"
+
+first_computation=$(( a + b * c ))
+second_computation=$(( a * b + c ))
+third_computation=$(( c + a / b ))
+fourth_computation=$(( a % b + c ))
+
+declare -A dictionary
+dictionary[first_computation]=$first_computation
+dictionary[second_computation]=$second_computation
+dictionary[third_computation]=$third_computation
+dictionary[fourth_computation]=$fourth_computation
+
+for computation in ${!dictionary[@]}
+do 
+    echo "$computation = ${dictionary[$computation]}"
+done
